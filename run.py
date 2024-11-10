6a41de16346dbc652c12310cd39d3edae3ab324c
VCGithubCode <vernellclark@hotmail.com>
2024-11-10 09:48:35 +0100

    Add boilerplate code to test deployment

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import time
+import json
+import os
+import signal
+
+# Color constants for terminal output
+MAGENTA = '\033[35m'
+RESET = '\033[0m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+WHITE = '\033[37m'
+
+print(f"{MAGENTA}Welcome to the game of war cards!{RESET}")
